afa554bd840551af892558dd955934fcaafe04b6
David Nonn <david.michael.nonn@gmail.com>
2021-08-08 08:42:53 -0700

    Test gpg sign in VS.

--- Contacts.API/Startup.cs
+++ Contacts.API/Startup.cs
@@ -1,15 +1,11 @@
 using Contacts.Application;
-using Contacts.Domain.Repositories;
 using Contacts.Infrastructure;
-using Contacts.Infrastructure.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using System.Reflection;
 
 namespace Contacts.API
 {
